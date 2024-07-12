3cf24bed743178773bab813faf763de7ade97b17
Light <102204002+aztice@users.noreply.github.com>
2024-07-12 21:37:27 +0800

    Create index.php

---
+++ api/db/GetKey/index.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/JsonDB.php');
+WebAPIAuth();
+$json = [
+    "status" => "success",
+    "code" => 200,
+    "message" => null,
+];
+http_response_code(200);
+$list = $_GET['list'];
+$jsonDB = new jsonDB();
+$jsonDB->Connect($_GET['dbname']);
+$value=$jsonDB->GetKey($list, $_GET['key']);
+if(isset($value)){
+    $json = [
+        "status" => "success",
+        "code" => 200,
+        "message" => $value
+    ];
+}
+echo json_encode($json);
