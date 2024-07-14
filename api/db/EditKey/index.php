1c3b656378a3c589dba2820a118f46e32d0a4604
Light <102204002+aztice@users.noreply.github.com>
2024-07-14 18:33:49 +0800

    Create index.php

---
+++ api/db/EditKey/index.php
@@ -0,0 +1,66 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/JsonDB.php');
+WebAPIAuth();
+$json = [
+    "status" => "error",
+    "code" => 400,
+    "message" => "Invalid format,Supported format are json,array,str",
+];
+http_response_code(400);
+$list = $_GET['list'];
+$jsonDB = new jsonDB();
+$jsonDB->Connect($_GET['dbname']);
+$jsonDB->WebAPI();
+if($_GET['format']!=='json' || $_GET['format']!=='array' || $_GET['format']!=='str'){
+    if (!isset($_GET['key']) || $_GET['key'] == '') {
+        $json = [
+            "status" => "error",
+            "code" => 400,
+            "message" => "Invalid key",
+        ];
+        http_response_code(400);
+        echo json_encode($json);
+        exit;
+    }
+    if($_GET['format']=='json'){
+        $data=json_decode($_GET['value'],true);
+        if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
+            $json = [
+                "status" => "error",
+                "code" => 400,
+                "message" => "Invalid format,please check your Json format",
+            ];
+            http_response_code(400);
+            echo json_encode($json);
+            exit;
+        }
+        
+    }
+    else if($_GET['format']=='array'){
+        $data=json_decode($_GET['value']);
+        if (!is_array($data) || json_last_error() !== JSON_ERROR_NONE) {
+            $json = [
+                "status" => "error",
+                "code" => 400,
+                "message" => "Invalid format,please check your Array format",
+            ];
+            http_response_code(400);
+            echo json_encode($json);
+            exit;
+        }
+    }
+    else{
+        $data = $_GET['value'];
+    }
+    $jsonDB->EditKey($_GET['list'],$_GET['key'],$data);
+    $json = [
+        "status" => "success",
+        "code" => 200,
+        "message" => null,
+    ];
+    http_response_code(200);
+    echo json_encode($json);
+    exit;
+}
+echo json_encode($json);
